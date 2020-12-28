e61603cd9566358138c38a8d63f90e2ce3858099
Pratik Shah <psh@narola.email>
2020-12-28 12:10:32 +0530

    Added API URL into environment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
     production: false,
     //   apiRoot: 'https://taskmanager-narola.herokuapp.com/api'
-    apiRoot: 'http://192.168.100.41:8083/api'
-    // apiRoot: 'http://clientapp.narola.online:2219/api'
+    // apiRoot: 'http://192.168.100.41:8083/api'
+    apiRoot: 'http://clientapp.narola.online:2219/api'
 };
 
 /*

76b4935c8f7ba2e522b7b077533b9d4cfc943c3f
Riya Shastri <ris@narola.email>
2021-01-15 14:08:48 +0530

    -->Solved routing issue , which we are facing on live application

--- src/app/core/guard/authentication.guard.ts
+++ src/app/core/guard/authentication.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/auth/service/auth.service';
 
 @Injectable({
     providedIn: 'root'
@@ -8,7 +9,8 @@ import { Observable } from 'rxjs';
 export class AuthenticationGuard implements CanActivate {
 
     constructor(
-        private router: Router
+        private router: Router,
+        private authService: AuthService
     ) { }
 
     canActivate(
@@ -18,7 +20,8 @@ export class AuthenticationGuard implements CanActivate {
         if (token !== null) {
             return true;
         } else {
-            this.router.navigate(['/']);
+            this.authService.logout();
+            this.router.navigate(['/login']);
             return false;
         }
     }
